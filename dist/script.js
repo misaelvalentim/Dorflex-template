595d2ca9a0296c43bd598cf23ecdc3405f17eec0
Misael Valentim <misael.valentim98@gmail.com>
2021-10-15 11:39:41 -0300

    minificando html e javascript

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,1 @@
-let menuHamburger = document.querySelector('.header__hamburger');
-let nav = document.querySelector('.header__nav');
-
-menuHamburger.addEventListener('click', function() {
-    nav.classList.toggle('active');
-})
+let menuHamburger=document.querySelector('.header__hamburger');let nav=document.querySelector('.header__nav');menuHamburger.addEventListener('click',function(){nav.classList.toggle('active');})
